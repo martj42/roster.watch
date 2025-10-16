581fce3a971571fe84795759d89fb5d0f7717d81
Mart Jürisoo <jyrkza@gmail.com>
2025-10-16 16:35:09 +0300

    .

--- ufc.bat
+++
@@ -1,5 +0,0 @@
-cd C:/Users/mart/Dropbox/R/roster.watch
-Rscript track_ufc.R >> ufc_auto.log 2>&1
-git add log.html >> ufc_auto.log 2>&1
-git commit -m "update" >> ufc_auto.log 2>&1
-git push >> ufc_auto.log 2>&1
